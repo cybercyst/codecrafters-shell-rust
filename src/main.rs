6894ed55be3a2da6e21a65c0b6c90957e0f82ddf
Forrest Loomis <cybercyst@gmail.com>
2024-06-08 19:19:30 -0400

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,8 @@
 use std::io::{self, Write};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
     let stdin = io::stdin();
