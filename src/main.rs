7bf0624cb83dd56b602e1b600f0886d0c5cbcb9a
Forrest Loomis <cybercyst@gmail.com>
2024-06-08 19:25:16 -0400

    refactor: use trim

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,5 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.strip_suffix('\n').unwrap())
+    println!("{}: command not found", input.trim())
 }
