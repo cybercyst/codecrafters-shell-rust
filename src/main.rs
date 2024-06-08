0b52fcf88d29a4d054acc2553152585f1787cc50
Forrest Loomis <cybercyst@gmail.com>
2024-06-08 19:24:50 -0400

    feat: handle missing commands

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,6 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    println!("{}: command not found", input.strip_suffix('\n').unwrap())
 }
