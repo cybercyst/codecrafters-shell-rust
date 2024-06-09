59739ae7c103a1a20f1f6bb2b92446002e8be4f9
Forrest Loomis <cybercyst@gmail.com>
2024-06-08 20:25:58 -0400

    feat: the exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
-fn main() {
+fn main() -> ! {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        eprintln!("{}: command not found", input.trim())
+
+        let mut tokens = input.trim().split(' ');
+        while let Some(token) = tokens.next() {
+            match token {
+                "exit" => {
+                    let exit_code = tokens.next().unwrap().parse::<i32>().unwrap();
+                    exit(exit_code)
+                }
+                _ => {
+                    eprintln!("{}: command not found", token);
+                    exit(1);
+                }
+            }
+        }
     }
 }
